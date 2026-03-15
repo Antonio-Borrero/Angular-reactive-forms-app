8540be389ec179474c1e24d2249561570a3d5cab
Antonio <antojoseabad@gmail.com>
2026-03-15 10:52:22 +0100

    chore: set up initial project structure

---
+++ src/app/reactive/pages/dynamic-page/dynamic-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-page',
+  imports: [],
+  templateUrl: './dynamic-page.html',
+})
+
+export class DynamicPage { }
---
+++ src/app/shared/components/side-menu/side-menu.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-menu',
+  imports: [],
+  templateUrl: './side-menu.html',
+  styleUrl: './side-menu.css',
+})
+
+export class SideMenu { }
