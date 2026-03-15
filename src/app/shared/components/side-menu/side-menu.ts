e02926d042caa6a8d8be678a119b0d1f5bd74ec0
Antonio <antojoseabad@gmail.com>
2026-03-15 11:38:08 +0100

    feat: add navigation menu

--- src/app/shared/components/side-menu/side-menu.ts
+++ src/app/shared/components/side-menu/side-menu.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { reactiveRoutes } from '../../../reactive/reactive.routes';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuItem {
+  title: string;
+  route: string;
+}
+
+const reactiveItems = reactiveRoutes[0].children ?? [];
 
 @Component({
   selector: 'app-side-menu',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu.html',
-  styleUrl: './side-menu.css',
 })
+export class SideMenu {
+  reactiveMenu: MenuItem[] = reactiveItems
+    .filter((item) => item.path !== '**')
+    .map((item) => ({
+      route: `reactive/${item.path}`,
+      title: `${item.title}`,
+    }));
 
-export class SideMenu { }
+    authMenu: MenuItem[] = [{
+      title: "Registro",
+      route: "./auth"
+    }]
+
+    countryMenu: MenuItem[] = [{
+      title: "Países",
+      route: "./country"
+    }]
+}
