76143cbb45b71b665c4fb5673e8fa071ef203002
Antonio <antojoseabad@gmail.com>
2026-03-21 13:07:35 +0100

    feat: implement registration form

--- src/app/auth/pages/register-page/register-page.ts
+++ src/app/auth/pages/register-page/register-page.ts
@@ -1,10 +1,32 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-register-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './register-page.html',
 })
 
-export class RegisterPage { }
+export class RegisterPage { 
+
+  fb = inject(FormBuilder);
+  formUtils = FormUtils;
+
+  myForm: FormGroup = this.fb.group({
+    name: ["", Validators.required],
+    email: ["", [Validators.required, Validators.email]],
+    userName: ["", [Validators.required, Validators.minLength(6)]],
+    password: ["", [Validators.required, Validators.minLength(6)]],
+    password2: [""],
+  })
+
+  onSubmit(){
+    if (this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    this.myForm.reset();
+  }
+}
--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -11,6 +11,8 @@ export class FormUtils {
           return `Mínimo de ${errors['minlength'].requiredLength} caracteres`;
         case 'min':
           return `Valor mínimo de ${errors['min'].min}`;
+        case 'email':
+          return `El correo ingresado no es un correo valido`;
       }
     }
     return null;
