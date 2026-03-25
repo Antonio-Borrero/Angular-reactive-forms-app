62e047b5db8001f86c5e17cbfceb9a6ed1903ac5
Antonio <antojoseabad@gmail.com>
2026-03-25 18:33:06 +0100

    feat: add regex-based validations to form fields

--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,6 +1,9 @@
 import { FormArray, FormGroup, ValidationErrors } from '@angular/forms';
 
 export class FormUtils {
+  static namePattern = '([a-zA-Z]+) ([a-zA-Z]+)';
+  static emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  static notOnlySpacesPattern = '^[a-zA-Z0-9]+$';
 
   static getTextErrors(errors: ValidationErrors) {
     for (const key of Object.keys(errors)) {
@@ -13,16 +16,22 @@ export class FormUtils {
           return `Valor mínimo de ${errors['min'].min}`;
         case 'email':
           return `El correo ingresado no es un correo valido`;
+        case 'pattern':
+          if (errors['pattern'].requiredPattern === FormUtils.emailPattern) {
+            return 'El valor ingresado no parece un correo electrónico';
+          }
+          return 'Error de patron contra expresión regular';
+
+        default:
+          return `Error de validación no controlado ${key}`;
       }
     }
     return null;
   }
-  
 
   static isValidField(form: FormGroup, fieldName: string): Boolean | null {
     return !!form.controls[fieldName].errors && form.controls[fieldName].touched;
   }
-
 
   static getFieldError(form: FormGroup, fieldName: string): string | null {
     if (!form.controls[fieldName]) return null;
@@ -32,12 +41,10 @@ export class FormUtils {
     return this.getTextErrors(errors);
   }
 
-
   static isValidFieldInArray(formArray: FormArray, index: number) {
     return formArray.controls[index].errors && formArray.controls[index].touched;
   }
 
-  
   static getFieldErrorInArray(formArray: FormArray, index: number): string | null {
     if (formArray.controls.length === 0) return null;
 
